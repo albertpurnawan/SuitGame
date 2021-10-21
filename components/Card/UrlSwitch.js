cb37a0224f64afa63ad0f25765af1c232814648d
unknown <albertpurnawan1@gmail.com>
2021-10-22 06:12:52 +0700

    first commit

--- components/Card/UrlSwitch.js
+++ components/Card/UrlSwitch.js
@@ -3,7 +3,7 @@ const UrlSwitch = () => {
 
   switch (link) {
     case "server":
-      return "https://server-suit-game.herokuapp.com";
+      return "https://suit-game.herokuapp.com";
     case "local":
       return "http://localhost:8000";
     default:
